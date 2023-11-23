91968fd9a93957a4e7c0f60e807221f97350bb4f
Ihor Optymist <ihor.optymist@gmail.com>
2023-11-23 12:12:57 +0100

    7.1 ctry and catch

---
+++ JavaScript/Okten_free_curse/7.1 try and catch.js
@@ -0,0 +1,20 @@
+//  
+
+
+// function calc(a, b) {
+//     if (b === 0) {
+//         throw new Error('b is 0')
+//     }
+//     return a / b
+// }
+
+// calc(10, 0);
+
+
+
+let user = {
+    name: 'Ivanov',
+};
+user?.name?.firstName; 
+
+// одноокий Елвис ?  )))
